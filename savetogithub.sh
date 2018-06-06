ba51abfc3968845ff51185f66e632066f7552a8b
Merofine <704783475@qq.com>
2018-06-06 06:21:09 +0000

    new note

---
+++ savetogithub.sh
@@ -0,0 +1,13 @@
+cd ~/Tx2note
+
+echo "git add new thing to the tree ..." 
+
+git add .
+
+echo "git commit new note to the local respository ..."
+
+git commit -m "new note"
+
+echo "git them to instant respository ..."
+
+git push -u Tx2note master
